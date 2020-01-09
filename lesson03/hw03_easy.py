a155f045aa378da3ae184d1bee0d01c448e5bb00
VilliWonka <v-u-simon@yandex.ru>
2020-01-09 22:46:42 +0300

    добавлено решение задач нормал и изи к третему уроку

--- lesson03/hw03_easy.py
+++ lesson03/hw03_easy.py
@@ -4,9 +4,24 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
-def my_round(number, ndigits):
-    pass
+"""Тут неободимо:
+1. Выделение делением на 10
+2. Определение через +/-"""
 
+
+
+def my_round(number, ndigits):
+    ten = 1
+    for i in range(ndigits):
+        ten = ten*10
+    number = number * ten
+    otb = int(number % 1 * 10)
+    if otb >= 6:
+        number += 1
+    number = int(number) / ten
+    return number
+
+# round_list.append(number % 10)
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
@@ -20,7 +35,14 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    ticket_list = []
+    while ticket_number > 0:
+        ticket_list.append(ticket_number % 10)
+        ticket_number = ticket_number // 10
+    if sum(ticket_list[:3]) == sum(ticket_list[3:]):
+        return 'билет счастливый'
+    else:
+        return 'билет НЕ счастливый'
 
 
 print(lucky_ticket(123006))
